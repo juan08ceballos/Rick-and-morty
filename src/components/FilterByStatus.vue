2d48e675697513cb0269ce62c707a7f019264674
Juan Ceballos <juan08ceballos@hotmail.com>
2021-10-08 13:18:08 -0500

    proyecto rym

---
+++ src/components/FilterByStatus.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="filter">
+      <div class="item" @click="filter('')">ALL</div>
+      <div class="item" @click="filter('Alive')">ALIVE</div>
+      <div class="item" @click="filter('Dead')">DEAD</div>
+      <div class="item" @click="filter('unknown')">UNKNOWN</div>
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex'
+export default {
+    setup(){
+        const store = useStore()
+
+        const filter = ((status) => {
+            store.dispatch('filterByStatus', status)
+        })
+
+        return {
+            filter
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.filter {
+  width: 400px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  border-radius: 10px;
+  overflow: hidden;
+  .item {
+    padding: 1rem 0.5rem;
+    background-color: var(--background-card);
+    text-align: center;
+    cursor: pointer;
+    &:hover {
+      color: var(--text-orange);
+    }
+  }
+}
+</style>
